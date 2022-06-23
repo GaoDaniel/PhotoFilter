9e9e077a5c409e796bf11f312c20b92ded39786a
GaoDaniel <35845523+GaoDaniel@users.noreply.github.com>
2022-06-23 15:43:38 -0700

    Some warning don't know fix, added take image/select image from image-picker

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,33 +1,102 @@
-import { StyleSheet } from 'react-native';
+import * as React from 'react';
+import {StyleSheet, SafeAreaView, View, Image, ScrollView} from 'react-native';
+import {CustomButton} from '../components/CustomButton';
+import {Title} from '../components/Title';
+import {Response} from '../components/Response';
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+import * as ImagePicker from 'react-native-image-picker';
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+/* toggle includeExtra */
+const includeExtra = true;
+
+export default function TabOneScreen() {
+  const [response, setResponse] = React.useState<any>(null);
+
+  const onButtonPress = React.useCallback((type, options) => {
+    if (type === 'capture') {
+      ImagePicker.launchCamera(options, setResponse);
+    } else {
+      ImagePicker.launchImageLibrary(options, setResponse);
+    }
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <Text>test test</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Title> PhotoFilter</Title>
+      <ScrollView>
+        <View style={styles.buttonContainer}>
+          {actions.map(({title, type, options}) => {
+            return (
+              <CustomButton
+                key={title}
+                onPress={() => onButtonPress(type, options)}>
+                {title}
+              </CustomButton>
+            );
+          })}
+        </View>
+        <Response>{response}</Response>
+
+        {response?.assets &&
+          response?.assets.map((uri: string) => (
+            <View key={uri} style={styles.image}>
+              <Image
+                resizeMode="cover"
+                resizeMethod="scale"
+                style={{width: 200, height: 200}}
+                source={{uri: uri}}
+              />
+            </View>
+          ))}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'aliceblue',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginVertical: 8,
+  },
+
+  image: {
+    marginVertical: 24,
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
   },
 });
+
+interface Action {
+  title: string;
+  type: 'capture' | 'library';
+  options: ImagePicker.CameraOptions | ImagePicker.ImageLibraryOptions;
+}
+
+const actions: Action[] = [
+  {
+    title: 'Take Image',
+    type: 'capture',
+    options: {
+      saveToPhotos: true,
+      mediaType: 'photo',
+      includeBase64: false,
+      includeExtra,
+    },
+  },
+  {
+    title: 'Select Image',
+    type: 'library',
+    options: {
+      maxHeight: 200,
+      maxWidth: 200,
+      selectionLimit: 0,
+      mediaType: 'photo',
+      includeBase64: false,
+      includeExtra,
+    },
+  },
+];
