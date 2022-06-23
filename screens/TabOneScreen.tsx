ec877e9157dcee2d66871d66fcb8efbcb4008633
edward-qin <3dwardqin@gmail.com>
2022-06-22 20:40:17 -0700

    initial test commit

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -8,6 +8,7 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
+      <Text>test test</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
