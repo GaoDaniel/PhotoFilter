3929e0910f1a3555f017ea2ea822cbe30e3da37a
GaoDaniel <35845523+GaoDaniel@users.noreply.github.com>
2022-06-24 16:03:08 -0700

    Tab two has expo-image-picker stuff

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,31 +1,69 @@
-import { StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Image, View, TouchableOpacity, StyleSheet, Text } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+export default function ImagePickerExample() {
+  const [image, setImage] = useState(null);
 
-export default function TabTwoScreen() {
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+  
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+  
+    let pickerResult = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+    if(pickerResult.cancelled == true){
+      return;
+    }
+    setImage({localUri: pickerResult.uri});
+  }
+  
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
+        <Text style={styles.buttonText}>Pick a photo</Text>
+      </TouchableOpacity>
+      {image && <Image source={{ uri: image.localUri }} style={styles.image} />}
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  title: {
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 20,
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
     fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+    color: '#fff',
+  }, 
+  image: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain",
+  }
 });
