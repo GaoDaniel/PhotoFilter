0a56c155e4c7d93f4bf91c2561891fa342a7f551
edward-qin <3dwardqin@gmail.com>
2022-06-30 22:34:43 -0700

    Some ScrollView added to handle large images, code cleanup Note: large images no longer appear in Image element

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -4,7 +4,6 @@ import * as ImagePicker from 'expo-image-picker';
 
 export default function ImagePickerExample() {
   const [image, setImage] = useState<ImagePicker.ImageInfo | null>(null);
-  const [data, setData] = useState<Uint8ClampedArray | null>(null);
   const [imageData, setImageData] = useState<ImageData | null>(null);
 
   const canvas = document.createElement('canvas');
@@ -26,12 +25,10 @@ export default function ImagePickerExample() {
       aspect: [4, 3],
       quality: 1,
     });
-    if(pickerResult.cancelled){
-      return;
+    if(!pickerResult.cancelled){
+      setImage(pickerResult);
+      console.log(image);
     }
-
-    setImage(pickerResult);
-    console.log(image);
   }
 
   let openCamera = async () => {
@@ -48,33 +45,32 @@ export default function ImagePickerExample() {
       aspect: [4, 3],
       quality: 1,
     });
-    if(result.cancelled){
-      return;
+    if(!result.cancelled){
+      setImage(result);
+      console.log(image);
     }
-    setImage(result);
-    console.log(image);
   }
 
-  function displayMatrix() {
-    const w = image.width;
-    const h = image.height;
+  function getMatrix() {
+    if (image) {
+      const w = image.width;
+      const h = image.height;
 
-    
-    canvas.width = w;
-    canvas.height = h;
-    console.log(w + ' ' + h);
+      canvas.width = w;
+      canvas.height = h;
+      console.log(w + ' ' + h);
 
-    const img = document.createElement('img');
-    img.src = image.uri;
-    img.onload = () => {
-      ctx.fillStyle = "red";
-      ctx.drawImage(img, 0, 0, w, h);
-      ctx.fillRect(10, 10, 500, 500);
-      const tempData = ctx.getImageData(0, 0, w, h);
-      setData(tempData.data);
-      setImageData(tempData);
-      console.log(tempData);
-    };
+      const img = document.createElement('img');
+      img.src = image.uri;
+      img.onload = () => {
+        if (ctx) {
+          ctx.drawImage(img, 0, 0, w, h);
+          const tempData = ctx.getImageData(0, 0, w, h);
+          setImageData(tempData);
+          console.log(tempData);
+        }
+      };
+    }
   }
 
   function backToImage() {
@@ -92,33 +88,42 @@ export default function ImagePickerExample() {
 
   return (
     <View style={styles.container}>
-    < ScrollView>
-      <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
-        <Text style={styles.buttonText}>Pick a photo</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={openCamera} style={styles.button}>
-        <Text style={styles.buttonText}>Take a photo</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={displayMatrix} style={styles.button}>
-        <Text style={styles.buttonText}>Display Matrix</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={backToImage} style={styles.button}>
-        <Text style={styles.buttonText}>Back to Image</Text>
-      </TouchableOpacity>
-      {image && <Image source={{ uri: image.uri }} style={styles.image} />}
-      <div>
-      <canvas
-        id="canvas"
-        ref={canvasRef}
-        width={image?.width}
-        height={image?.height}
-        style={{
-          border: '2px solid #000',
-          marginTop: 10,
-        }}
-      ></canvas>
-      </div>
-    </ScrollView>
+      <ScrollView horizontal={true} minimumZoomScale={0.5} maximumZoomScale={2} pinchGestureEnabled={true} showsHorizontalScrollIndicator={true}>
+        <ScrollView minimumZoomScale={0.5} maximumZoomScale={2} pinchGestureEnabled={true} showsVerticalScrollIndicator={true}>
+          <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
+            <Text style={styles.buttonText}>Pick a photo</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={openCamera} style={styles.button}>
+            <Text style={styles.buttonText}>Take a photo</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={getMatrix} style={styles.button}>
+            <Text style={styles.buttonText}>Display Matrix</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={backToImage} style={styles.button}>
+            <Text style={styles.buttonText}>Back to Image</Text>
+          </TouchableOpacity>
+          {image && <Image source={{ uri: image.uri }} style={styles.image} />}
+          <View style={{height: 500, width: 500}}>
+            <ScrollView maximumZoomScale={3} minimumZoomScale={0.05} pinchGestureEnabled={true} showsVerticalScrollIndicator={true}>
+              <ScrollView horizontal={true} maximumZoomScale={3} minimumZoomScale={0.05} pinchGestureEnabled={true} showsHorizontalScrollIndicator={true}>
+                <div>
+                  <canvas
+                      id="canvas"
+                      ref={canvasRef}
+                      width={image?.width}
+                      height={image?.height}
+                      style={{
+                        border: '2px solid #000',
+                        marginTop: 10,
+                      }}
+                  />
+                </div>
+              </ScrollView>
+            </ScrollView>
+          </View>
+        </ScrollView>
+      </ScrollView>
+
     </View>
   );
 }
@@ -144,6 +149,9 @@ const styles = StyleSheet.create({
     backgroundColor: "blue",
     padding: 20,
     borderRadius: 5,
+    border: '2px solid #000',
+    marginBottom: 5,
+    marginTop: 5,
   },
   buttonText: {
     fontSize: 20,
