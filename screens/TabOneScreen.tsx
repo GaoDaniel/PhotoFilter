b2ae287cb88a05b1a71b24d41168e0d9351b1383
GaoDaniel <35845523+GaoDaniel@users.noreply.github.com>
2022-07-07 16:22:38 -0700

    solved????

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,54 +1,29 @@
 import * as React from 'react';
+import {useState}  from 'react';
 import {StyleSheet, SafeAreaView, View, Image, ScrollView} from 'react-native';
-import {CustomButton} from '../components/CustomButton';
-import {Title} from '../components/Title';
-import {Response} from '../components/Response';
-
-import * as ImagePicker from 'react-native-image-picker';
-
-/* toggle includeExtra */
-const includeExtra = true;
+import {Camera, CameraPermissionStatus, useCameraDevices} from 'react-native-vision-camera';
 
 export default function TabOneScreen() {
-  const [response, setResponse] = React.useState<any>(null);
+  const devices = useCameraDevices();
+  const device = devices.back;
+  const [active, setActive] = useState(false);
 
-  const onButtonPress = React.useCallback((type, options) => {
-    if (type === 'capture') {
-      ImagePicker.launchCamera(options, setResponse);
-    } else {
-      ImagePicker.launchImageLibrary(options, setResponse);
+  let openCamera = async () => {
+    let permissionResult = await Camera.requestCameraPermission();
+
+    if (permissionResult != 'authorized') {
+      return;
     }
-  }, []);
-
+    setActive(true);
+  }
   return (
     <SafeAreaView style={styles.container}>
-      <Title> PhotoFilter</Title>
-      <ScrollView>
-        <View style={styles.buttonContainer}>
-          {actions.map(({title, type, options}) => {
-            return (
-              <CustomButton
-                key={title}
-                onPress={() => onButtonPress(type, options)}>
-                {title}
-              </CustomButton>
-            );
-          })}
-        </View>
-        <Response>{response}</Response>
-
-        {response?.assets &&
-          response?.assets.map((uri: string) => (
-            <View key={uri} style={styles.image}>
-              <Image
-                resizeMode="cover"
-                resizeMethod="scale"
-                style={{width: 200, height: 200}}
-                source={{uri: uri}}
-              />
-            </View>
-          ))}
-      </ScrollView>
+      <Camera 
+        style={StyleSheet.absoluteFill}
+        device={device}
+        photo={true}
+        isActive={active}
+      />
     </SafeAreaView>
   );
 }
@@ -69,34 +44,3 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
 });
-
-interface Action {
-  title: string;
-  type: 'capture' | 'library';
-  options: ImagePicker.CameraOptions | ImagePicker.ImageLibraryOptions;
-}
-
-const actions: Action[] = [
-  {
-    title: 'Take Image',
-    type: 'capture',
-    options: {
-      saveToPhotos: true,
-      mediaType: 'photo',
-      includeBase64: false,
-      includeExtra,
-    },
-  },
-  {
-    title: 'Select Image',
-    type: 'library',
-    options: {
-      maxHeight: 200,
-      maxWidth: 200,
-      selectionLimit: 0,
-      mediaType: 'photo',
-      includeBase64: false,
-      includeExtra,
-    },
-  },
-];
