e89faa0613d47bda9c8a9c85475a25ea2897f069
edward-qin <3dwardqin@gmail.com>
2022-07-20 18:20:03 -0700

    fixed server connection blur

--- server/src/main/java/SparkServer.java
+++ server/src/main/java/SparkServer.java
@@ -23,7 +23,6 @@ public class SparkServer {
 
         // filter request
         Spark.post("/filtering", (request, response) -> {
-            System.out.println("got here");
             String uri = request.body();
             String filter = request.queryParams("filter");
 
@@ -31,7 +30,7 @@ public class SparkServer {
                 Spark.halt(400, "missing one of uri or filter");
             }
             filter = filter.toLowerCase();
-            if (!filters.contains(filter)){
+            if (!filters.contains(filter)) {
                 Spark.halt(401, "filter does not exist");
             }
             uri = uri.replace(' ', '+');
@@ -46,7 +45,6 @@ public class SparkServer {
             // get bytes from base64
             byte[] imageData = new byte[0];
             try {
-                System.out.println(uri.substring(contentStartIndex));
                 imageData = Base64.getDecoder().decode(uri.substring(contentStartIndex));
             } catch (IllegalArgumentException e) {
                 Spark.halt(403, "invalid base64 scheme");
@@ -57,12 +55,12 @@ public class SparkServer {
 
             // get BufferedImage
             BufferedImage inputImage = ImageIO.read(new ByteArrayInputStream(imageData));
-            if (inputImage == null){
+            if (inputImage == null) {
                 Spark.halt(405, "base64 could not be read");
             }
 
             // filter
-            switch (filter){
+            switch (filter) {
                 case "invert":
                     invert(inputImage);
                     break;
@@ -91,22 +89,22 @@ public class SparkServer {
     }
 
     // filters represented by private methods
-    private static void invert(BufferedImage image){
-        for (int i = 0; i < image.getWidth(); i++){
-            for (int j = 0; j < image.getHeight(); j++){
+    private static void invert(BufferedImage image) {
+        for (int i = 0; i < image.getWidth(); i++) {
+            for (int j = 0; j < image.getHeight(); j++) {
                 int argb = image.getRGB(i, j);
                 int alpha = 0xFF & (argb >> 24);
-                int red = 255 - (0xFF & ( argb >> 16));
-                int green = 255 - (0xFF & (argb >> 8 ));
+                int red = 255 - (0xFF & (argb >> 16));
+                int green = 255 - (0xFF & (argb >> 8));
                 int blue = 255 - (0xFF & argb);
-                image.setRGB(i, j, ((alpha << 24) | (red << 16 ) | (green<<8) | blue));
+                image.setRGB(i, j, ((alpha << 24) | (red << 16) | (green << 8) | blue));
             }
         }
     }
 
-    private static void hflip(BufferedImage image){
-        for(int i = 0; i < image.getWidth()/2; i++){
-            for(int j = 0; j < image.getHeight(); j++){
+    private static void hflip(BufferedImage image) {
+        for (int i = 0; i < image.getWidth() / 2; i++) {
+            for (int j = 0; j < image.getHeight(); j++) {
                 int temp = image.getRGB(i, j);
                 image.setRGB(i, j, image.getRGB(image.getWidth() - i - 1, j));
                 image.setRGB(image.getWidth() - i - 1, j, temp);
@@ -114,9 +112,9 @@ public class SparkServer {
         }
     }
 
-    private static void vflip(BufferedImage image){
-        for(int i = 0; i < image.getWidth(); i++){
-            for(int j = 0; j < image.getHeight()/2; j++){
+    private static void vflip(BufferedImage image) {
+        for (int i = 0; i < image.getWidth(); i++) {
+            for (int j = 0; j < image.getHeight() / 2; j++) {
                 int temp = image.getRGB(i, j);
                 image.setRGB(i, j, image.getRGB(i, image.getHeight() - j - 1));
                 image.setRGB(i, image.getHeight() - j - 1, temp);
@@ -124,56 +122,45 @@ public class SparkServer {
         }
     }
 
-    private static void grayscale(BufferedImage image){
-        for (int i = 0;i < image.getWidth(); i++){
-            for (int j = 0; j < image.getHeight(); j++){
+    private static void grayscale(BufferedImage image) {
+        for (int i = 0; i < image.getWidth(); i++) {
+            for (int j = 0; j < image.getHeight(); j++) {
                 int argb = image.getRGB(i, j);
                 int alpha = 0xFF & (argb >> 24);
                 int red = 0xFF & (argb >> 16);
                 int green = 0xFF & (argb >> 8);
                 int blue = 0xFF & argb;
-                int gray = (red + green + blue)/3;
-                image.setRGB(i, j, ((alpha << 24) | (gray << 16 ) | (gray<<8) | gray));
+                int gray = (red + green + blue) / 3;
+                image.setRGB(i, j, ((alpha << 24) | (gray << 16) | (gray << 8) | gray));
             }
         }
     }
 
-    private static void blur(BufferedImage image){
-        if (image.getWidth() < 3 || image.getHeight() < 3){
+    private static void blur(BufferedImage image) {
+        if (image.getWidth() < 3 || image.getHeight() < 3) {
             return;
         }
-        int[][] copy = new int[image.getHeight() - 2][image.getWidth() - 2];
-        // optimized for rows (could probably be further optimized)
-        for (int i = 0; i < image.getHeight() - 2; i++){
-            int[][] a = new int[3][3]; // sort of like a mini cache
-            int[] sum = new int[3]; // sum of rgb
-            for (int k = 0; k < 3; k++) {
-                for (int j = 0; j < 2; j++) {
-                    a[k][j] = image.getRGB(k, j);
-                    sum[0] += 0XFF & (a[k][j] >> 16);
-                    sum[1] += 0XFF & (a[k][j] >> 8);
-                    sum[2] += 0xFF & a[k][j];
+        int[][] copy = new int[image.getWidth() - 2][image.getHeight() - 2];
+
+        for (int i = 1; i < image.getWidth() - 1; i++) {
+            for (int j = 1; j < image.getHeight() - 1; j++) {
+                int[] sum = new int[3];
+                for (int x = -1; x <= 1; x++){
+                    for (int y = -1; y <= 1; y++){
+                        int argb = image.getRGB(i+x, j+y);
+                        sum[0] += 0xFF & (argb >> 16);
+                        sum[1] += 0xFF & (argb >> 8);
+                        sum[2] += 0xFF & argb;
+                    }
                 }
-            }
-            for (int j = 0; j < image.getWidth() - 2; j++){
-                a[0][(j+2)%3] = image.getRGB(i + 2, j);
-                a[1][(j+2)%3] = image.getRGB(i + 2, j + 1);
-                a[2][(j+2)%3] = image.getRGB(i + 2, j + 2);
-                sum[0] += (0xFF & (a[0][(j+2)%3] >> 16)) - (0xFF & (a[1][(j+2)%3] >> 16)) - (0xFF & (a[2][(j+2)%3] >> 16));
-                sum[1] += (0xFF & (a[0][(j+2)%3] >> 8)) - (0xFF & (a[1][(j+2)%3] >> 8)) - (0xFF & (a[2][(j+2)%3] >> 8));
-                sum[2] += (0xFF & a[0][(j+2)%3]) - (0xFF & a[1][(j+2)%3]) - (0xFF & a[2][(j+2)%3]);
-
-                int alpha = 0xFF & (a[1][(j+1)%3] >> 24);
-                // values are rounded down
-                copy[i][j] = ((alpha << 24) | (sum[0]/9 << 16 ) | (sum[1]/9 <<8) | (sum[2]/9));
-
-                sum[0] -= (0xFF & (a[0][j%3] >> 16)) - (0xFF & (a[1][j%3] >> 16)) - (0xFF & (a[2][j%3] >> 16));
-                sum[1] -= (0xFF & (a[0][j%3] >> 8)) - (0xFF & (a[1][j%3] >> 8)) - (0xFF & (a[2][j%3] >> 8));
-                sum[2] -= (0xFF & a[0][j%3]) - (0xFF & a[1][j%3]) - (0xFF & a[2][j%3]);
+                int alpha = 0xFF & (image.getRGB(i,j) >> 24);
+                // floored rgb values
+                copy[i-1][j-1] = (alpha << 24) | (sum[0]/9 << 16) | (sum[1]/9 << 8) | (sum[2]/9);
             }
         }
-        for (int i = 0; i < copy.length; i++){
-            for (int j = 0; j < copy[0].length; j++){
+
+        for (int i = 0; i < copy.length; i++) {
+            for (int j = 0; j < copy[0].length; j++) {
                 image.setRGB(i + 1, j + 1, copy[i][j]);
             }
         }
